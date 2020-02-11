144292c8411a9732d9cb6516c0b8d6363f9f80dc
Aleksandr <aleksandrfoman@gmail.com>
2020-02-11 18:05:31 +0200

    Начальная сцена, папки, камера

---
+++ RTS_3D/Assets/Scripts/CController.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+
+public class CController : MonoBehaviour
+{
+    public float speed = 0f;
+
+    private Vector3 _pos = Vector3.zero;
+    void Update()
+    {
+        if (Input.mousePosition.x >= (Screen.width - 2.25f))
+        {
+            _pos.x = speed * Time.deltaTime;
+        }
+        else if (Input.mousePosition.x <= 2.25f)
+        {
+            _pos.x = -(speed * Time.deltaTime);
+        }
+        else
+        {
+            _pos.x = 0f;
+        }
+        if (Input.mousePosition.y >= (Screen.height - 2.25f))
+        {
+            _pos.z = speed * Time.deltaTime;
+        }
+        else if (Input.mousePosition.y <= 2.25f)
+        {
+            _pos.z = -(speed * Time.deltaTime);
+        }
+        else
+        {
+            _pos.z = 0f;
+        }
+        transform.position += _pos;
+    }
+}
---
+++ RTS_3D/Assets/Scripts/PController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PController : MonoBehaviour
+{
+    private Camera _cam = null;
+
+    void Start()
+    {
+        _cam = Camera.main;
+    }
+
+    void Update()
+    {
+        
+    }
+}
