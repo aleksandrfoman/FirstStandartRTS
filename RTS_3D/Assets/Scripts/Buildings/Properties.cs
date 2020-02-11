fea21cbae45f9c38cd44cd89a0a279aa7accf5ef
Aleksandr <aleksandrfoman@gmail.com>
2020-02-11 23:18:04 +0200

    тест

---
+++ RTS_3D/Assets/Scripts/Buildings/Properties.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Properties : MonoBehaviour
+{
+    //Для коммита
+    public string Name = "Имени нету";
+
+    public float maxHealth = 0f;
+    public float curHealth = 0f;
+
+    public TypeBuildings Type;
+
+
+}
