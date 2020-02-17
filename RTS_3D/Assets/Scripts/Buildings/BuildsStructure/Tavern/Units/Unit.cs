8902fdc2c94c75c71a83296b8ec50d7f0b1f27f0
Aleksandr <aleksandrfoman@gmail.com>
2020-02-17 12:42:22 +0200

    NavMesh для Юнитов

--- RTS_3D/Assets/Scripts/Buildings/BuildsStructure/Tavern/Units/Unit.cs
+++ RTS_3D/Assets/Scripts/Buildings/BuildsStructure/Tavern/Units/Unit.cs
@@ -1,18 +1,41 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class Unit : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [Header("Descriptio")]
+    public string Name = "None";
+
+    public float maxHealth = 0f;
+    public float curHealth = 0f;
+    public float Speed = 0f;
+
+    private NavMeshAgent p_agent = null;
+
+    private Camera p_camera;
+    private Ray _ray;
+    private RaycastHit _hit;
+
+
+    private void Start()
     {
-        
+        p_camera = Camera.main;
+        p_agent = GetComponent<NavMeshAgent>();
+        p_agent.speed = Speed;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        if (Input.GetMouseButton(1))
+        {
+            _ray = p_camera.ScreenPointToRay(Input.mousePosition);
+            if(Physics.Raycast(_ray,out _hit))
+            {
+                p_agent.SetDestination(_hit.point);
+            }
+        }
     }
+
 }
